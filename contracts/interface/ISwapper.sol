e67e444261f63c45922970be395a1c059f32e0c5
robin-h-liu <robin_h_liu@trendmicro.com>
2023-07-09 01:34:34 +0800

    feat: add swapper interface

---
+++ contracts/interface/ISwapper.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+import {BalanceDelta} from "v4-core/types/BalanceDelta.sol";
+
+interface ISwapper {
+   function swap(
+        IPoolManager.PoolKey memory key,
+        IPoolManager.SwapParams memory params
+    ) external payable returns (BalanceDelta); 
+}
